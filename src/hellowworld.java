ec43042a7ae2ab52e99e996ca8bdf1310c49fc4e
USER <USER@222.96.178.152>
2020-03-24 17:22:38 +0900

    creat a menu

---
+++ src/hellowworld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		 System.out.println("1. Add new cup-ramen");
+		 System.out.println("1. Delete cup-ramen");
+		 System.out.println("1. Edit cup-ramen");
+		 System.out.println("1. view new cup-ramen");
+		 System.out.println("1. Exit");
+
+}
+}
